f5bf737ab0c5524e221742b860421e2a84b30c01
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2021-12-03 09:31:28 -0500

    fixed eclipse dmg download link

--- eclipse-modeling.rb
+++ eclipse-modeling.rb
@@ -2,7 +2,7 @@ cask 'eclipse-modeling' do
   version '4.17.0,2020-09'
   sha256 '4121cfdf9390ff94fd1a2812cf3296c76f121ec4a3402e01aca514c58c8b2b0b'
 
-  url "http://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/2020-09/R/eclipse-modeling-2020-09-R-macosx-cocoa-x86_64.dmg"
+  url "https://www.cs.mcgill.ca/~mschie3/eclipse-modeling-2020-09-R-macosx-cocoa-x86_64.dmg"
   name 'Eclipse Modeling Tools'
   homepage 'https://eclipse.org/'
 
